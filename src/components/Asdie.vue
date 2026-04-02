d36447d12de8422e1a22c7abe0a8a8d392c15928
hingq <97820978+hingq@users.noreply.github.com>
2026-04-02 17:19:39 +0800

    feat: add icons to algorithm sidebar sections

--- src/components/Asdie.vue
+++ src/components/Asdie.vue
@@ -1,51 +1,77 @@
 <template>
   <el-menu default-active="2" :collapse="isCollapse" background-color="var(--vt-c-blue)" :router="true" :mode=mode>
-    <!-- <template v-for="i in 3" :key="i">
-      <el-menu-item></el-menu-item>
-      <el-sub-menu v-for="i in 2" :key=i index="i">
-        <template #title>
-          <el-icon><icon-menu /></el-icon>
-          <span>课程信息</span>
-        </template>
-<template v-for="j in 2" :key="j">
-          <el-menu-item>j{{ j }}</el-menu-item>
-        </template>
-</el-sub-menu>
-</template> -->
-
     <el-menu-item index="/layout">
       <el-icon>
-        <location />
+        <UserFilled />
       </el-icon>
       <template #title>
         <span>个人信息</span>
       </template>
     </el-menu-item>
+
     <el-sub-menu index="/layout/course">
       <template #title>
-        <el-icon><icon-menu /></el-icon>
+        <el-icon>
+          <Reading />
+        </el-icon>
         <span>课程信息</span>
       </template>
-      <el-menu-item index="/layout/course/student">学生课表</el-menu-item>
-      <el-menu-item index="/layout/course/taken">已修课程</el-menu-item>
-      <el-menu-item index="/layout/course/select">学生选课</el-menu-item>
+      <el-menu-item index="/layout/course/student">
+        <el-icon>
+          <Calendar />
+        </el-icon>
+        <span>学生课表</span>
+      </el-menu-item>
+      <el-menu-item index="/layout/course/taken">
+        <el-icon>
+          <Checked />
+        </el-icon>
+        <span>已修课程</span>
+      </el-menu-item>
+      <el-menu-item index="/layout/course/select">
+        <el-icon>
+          <Select />
+        </el-icon>
+        <span>学生选课</span>
+      </el-menu-item>
     </el-sub-menu>
-    <el-sub-menu index="/layout/test">
 
+    <el-sub-menu index="/layout/test">
       <template #title>
         <el-icon>
-          <document />
+          <Document />
         </el-icon>
         <span>考试</span>
       </template>
-      <el-menu-item index="/layout/test/arrange">考试安排</el-menu-item>
-      <el-menu-item index="/layout/test/postpone">缓考申请</el-menu-item>
-      <el-menu-item index="/layout/test/exemption">免考申请</el-menu-item>
-      <el-menu-item index="/layout/test/evaluate">evaluate</el-menu-item>
+      <el-menu-item index="/layout/test/arrange">
+        <el-icon>
+          <List />
+        </el-icon>
+        <span>考试安排</span>
+      </el-menu-item>
+      <el-menu-item index="/layout/test/postpone">
+        <el-icon>
+          <Clock />
+        </el-icon>
+        <span>缓考申请</span>
+      </el-menu-item>
+      <el-menu-item index="/layout/test/exemption">
+        <el-icon>
+          <Tickets />
+        </el-icon>
+        <span>免考申请</span>
+      </el-menu-item>
+      <el-menu-item index="/layout/test/evaluate">
+        <el-icon>
+          <EditPen />
+        </el-icon>
+        <span>成绩评价</span>
+      </el-menu-item>
     </el-sub-menu>
+
     <el-menu-item index="4">
       <el-icon>
-        <setting />
+        <Setting />
       </el-icon>
       <template #title>Navigator Four</template>
     </el-menu-item>
@@ -53,31 +79,30 @@
 </template>
 
 <script setup>
-import { computed, } from 'vue'
+import { computed } from 'vue'
 import {
+  Calendar,
+  Checked,
+  Clock,
   Document,
-  Menu as IconMenu,
-  Location,
+  EditPen,
+  List,
+  Reading,
+  Select,
   Setting,
+  Tickets,
+  UserFilled,
 } from '@element-plus/icons-vue'
-// import { useCollaspeStore } from '@/stores/collaspe';
-import { useCollapse } from '@/stores';
+import { useCollapse } from '@/stores'
+
 const isCollapse = computed(() => useCollapse.iscollaspe)
 const mode = computed(() => useCollapse.rowOrCol)
-
-// const handleOpen = (key, keyPath) => {
-//   // console.log(key, keyPath)
-// }
-// const handleClose = (key, keyPath) => {
-//   // console.log(key, keyPath)
-// }
 </script>
 
 <style scoped>
 .el-sub-menu__title,
 .el-menu-item {
   --el-menu-hover-bg-color: #275880;
-
 }
 
 .el-menu {
